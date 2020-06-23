e9017067aa5df6d30eab6da94e87c736b5b32d5a
Jared Bruni <jaredbruni@gmail.com>
2020-06-23 00:27:05 -0700

    texture mapped video

---
+++ 015.Video.Texture/gl_shader.hpp
@@ -0,0 +1,18 @@
+#ifndef __GL_SHADER__
+#define __GL_SHADER__
+
+#include<iostream>
+#include<string>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+class ShaderProgram {
+public:
+    int printShaderLog(GLuint err);
+    void printProgramLog(int p);
+    bool checkError();
+    GLuint createProgram(const char *vert, const char *frag);
+    GLuint createProgramFromFile(const std::string &vert, const std::string &frag);
+};
+
+#endif
